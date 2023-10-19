22a07d74a4e7c945e2ae7cabc298b9b1312c35e9
Alisa Fedorenko <afedorenko00@gmail.com>
2023-10-19 14:10:24 +0300

    add main function from fastq_script

--- main_script.py
+++ main_script.py
@@ -3,6 +3,7 @@ from data_processing_scripts.das_protein_tools import main_protein_tools
 from data_processing_scripts.fastq_tools import main_fastq_tools
 
 
+
 # Call the main_dna_rna_tools function with the necessary arguments.
 result = main_dna_rna_tools("ATcg", "reverse")
 print(result)
@@ -25,3 +26,27 @@ filtered_sequences = main_fastq_tools(
 )
 
 print(filtered_sequences)
+
+def main(input_path: str, output_filename: str = None):
+    """
+    Process a FASTQ file, apply filtering, and save the filtered sequences to a new FASTQ file.
+
+    Args:
+        input_path (str): The path to the input FASTQ file.
+        output_filename (str, optional): The name for the output FASTQ file. If not provided, the input file name is used.
+
+    Returns:
+        None: This function does not return a value but saves the filtered sequences to a new FASTQ file.
+    """
+
+
+    parsed_data = parse_file(input_path)
+    filtered_seqs = main_fastq_tools(parsed_data)
+    if output_filename is None:
+        output_filename = input_path.split("/")[-1]
+    else:
+        output_filename = output_filename + ".fastq"
+
+    os.makedirs("./fastq_filtrator_resuls", exist_ok=True)
+    output_path = "./fastq_filtrator_resuls/" + output_filename
+    save_filtered_fastq(output_path, filtered_seqs)
