1099272149e6c86b84588caaf2f1fbe418a13a05
Alisa Fedorenko <afedorenko00@gmail.com>
2023-12-02 00:37:51 +0300

    Edit function names

--- main_script.py
+++ main_script.py
@@ -1,12 +1,14 @@
+import os
+
+import data_processing_scripts.dna_rna_dict as drd
+import data_processing_scripts.protein_dict as prd
+
 from data_processing_scripts.dna_rna_tools import transcribe, reverse, complement, reverse_complement
 from data_processing_scripts.das_protein_tools import get_pI, calculate_aa_freq, translate_protein_rna, convert_to_3L_code, protein_mass
 from data_processing_scripts.fastq_script import main_fastq_tools, parse_file, save_filtered_fastq
-import data_processing_scripts.dna_rna_dict as drd
-import data_processing_scripts.protein_dict as prd
-import os
 
 
-def main_dna_rna_tools(*args: str):
+def run_dna_rna_tools(*args: str):
     """
     Run various DNA/RNA sequence manipulation tools.
 
@@ -44,7 +46,7 @@ def main_dna_rna_tools(*args: str):
     return results if len(results) > 1 else results[0]
 
 
-def main_protein_tools(*args: str):
+def run_protein_tools(*args: str):
     """
     Main function to perform various actions on protein sequences.
 
@@ -88,7 +90,7 @@ def main_protein_tools(*args: str):
 
     return result
 
-def main_fastq(input_path: str, output_filename: str = None):
+def run_fastq(input_path: str, output_filename: str = None):
     """
     Process a FASTQ file, apply filtering, and save the filtered sequences to a new FASTQ file.
 
